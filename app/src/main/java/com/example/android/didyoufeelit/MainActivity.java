4fab86b3f9f0ecf6c17ab2e886d36b6171753864
Guilherme Fainer Dalben <guilherme.fainer@gmail.com>
2017-08-30 21:01:34 -0300

    Moved the network task to a separeted thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.net.URL;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -34,11 +37,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
 
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        EarthquakeAsyncTask earthquake = new EarthquakeAsyncTask();
+        earthquake.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -54,4 +55,33 @@ public class MainActivity extends AppCompatActivity {
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event>{
+
+        @Override
+        protected Event doInBackground(String... urls) {
+
+            if (urls==null || urls.length==0)
+                return null;
+
+            // Perform the HTTP request for earthquake data and process the response.
+            Event earthquake = Utils.fetchEarthquakeData(urls[0]);
+            return earthquake;
+        }
+
+
+        @Override
+        protected void onPostExecute(Event event) {
+            if (event == null)
+                return;
+
+            super.onPostExecute(event);
+
+            // Update the information displayed to the user.
+            updateUi(event);
+        }
+    }
+
+
+
 }
